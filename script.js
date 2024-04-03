1dd6300f5436d8f87f88683ba7e07f8e7bf83948
JC-ProgJava <juch5796@gmail.com>
2024-04-03 13:51:10 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+document.querySelectorAll(".link")[0].addEventListener("mouseover", () => {
+  document.body.style["background-color"] = "var(--1-secondary)";
+  document.body.style["color"] = "var(--1-primary)";
+  document.querySelector(".subscribe button").style["background-color"] = "var(--1-primary)";
+  document.querySelector(".subscribe button").style["color"] = "var(--1-secondary)";
+  document.querySelectorAll(".replaceLogo")[0].src = "assets/logo-i1.svg";
+  document.querySelectorAll(".replaceLogo")[1].src = "assets/logo-i1.svg";
+
+})
+
+document.querySelectorAll(".link")[1].addEventListener("mouseover", () => {
+  document.body.style["background-color"] = "var(--2-secondary)";
+  document.body.style["color"] = "var(--2-primary)";
+  document.querySelector(".subscribe button").style["background-color"] = "var(--2-primary)";
+  document.querySelector(".subscribe button").style["color"] = "var(--2-secondary)";
+  document.querySelectorAll(".replaceLogo")[0].src = "assets/logo-i2.svg";
+  document.querySelectorAll(".replaceLogo")[1].src = "assets/logo-i2.svg";
+})
+
+document.querySelectorAll("#colorTiles div")[0].addEventListener("mouseover", () => {
+  document.body.style["background-color"] = "var(--1-secondary)";
+  document.body.style["color"] = "var(--1-primary)";
+  document.querySelector(".subscribe button").style["background-color"] = "var(--1-primary)";
+  document.querySelector(".subscribe button").style["color"] = "var(--1-secondary)";
+  document.querySelectorAll(".replaceLogo")[0].src = "assets/logo-i1.svg";
+  document.querySelectorAll(".replaceLogo")[1].src = "assets/logo-i1.svg";
+})
+
+document.querySelectorAll("#colorTiles div")[1].addEventListener("mouseover", () => {
+  document.body.style["background-color"] = "var(--2-secondary)";
+  document.body.style["color"] = "var(--2-primary)";
+  document.querySelector(".subscribe button").style["background-color"] = "var(--2-primary)";
+  document.querySelector(".subscribe button").style["color"] = "var(--2-secondary)";
+  document.querySelectorAll(".replaceLogo")[0].src = "assets/logo-i2.svg";
+  document.querySelectorAll(".replaceLogo")[1].src = "assets/logo-i2.svg";
+})
+
+document.querySelectorAll("#colorTiles div")[2].addEventListener("mouseover", () => {
+  document.body.style["background-color"] = "var(--3-secondary)";
+  document.body.style["color"] = "var(--3-primary)";
+  document.querySelector(".subscribe button").style["background-color"] = "var(--3-primary)";
+  document.querySelector(".subscribe button").style["color"] = "var(--3-secondary)";
+  document.querySelectorAll(".replaceLogo")[0].src = "assets/logo-i3.svg";
+  document.querySelectorAll(".replaceLogo")[1].src = "assets/logo-i3.svg";
+})
