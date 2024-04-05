aa8bf89a007c94199fb4edea4373075e8fa20ab8
JC-ProgJava <juch5796@gmail.com>
2024-04-05 21:46:12 +0800

    Prepare push to github

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.querySelectorAll(".link")[0].addEventListener("mouseover", colorIssueOne)
 document.querySelectorAll(".link")[1].addEventListener("mouseover", colorIssueTwo)
-document.querySelectorAll(".link")[2].addEventListener("mouseover", colorIssueThree)
+//document.querySelectorAll(".link")[2].addEventListener("mouseover", colorIssueThree)
 // document.querySelectorAll(".link")[3].addEventListener("mouseover", colorIssueFour)
 
 document.querySelectorAll("#colorTiles div")[0].addEventListener("mouseover", colorIssueOne)
 document.querySelectorAll("#colorTiles div")[1].addEventListener("mouseover", colorIssueTwo)
 document.querySelectorAll("#colorTiles div")[2].addEventListener("mouseover", colorIssueThree)
-document.querySelectorAll("#colorTiles div")[3].addEventListener("mouseover", colorIssueFour)
+//document.querySelectorAll("#colorTiles div")[3].addEventListener("mouseover", colorIssueFour)
 
 function colorIssueOne() {
   document.body.style["background-color"] = "var(--1-secondary)";
@@ -38,6 +38,7 @@ function colorIssueThree() {
   document.querySelector("#footer").style["color"] = "var(--3-primary)";
 }
 
+/*
 function colorIssueFour() {
   document.body.style["background-color"] = "var(--4-secondary)";
   document.body.style["color"] = "var(--4-primary)";
@@ -47,3 +48,4 @@ function colorIssueFour() {
   document.querySelectorAll(".replaceLogo")[1].src = "assets/logo-i4.svg";
   document.querySelector("#footer").style["color"] = "var(--4-primary)";
 }
+ */
