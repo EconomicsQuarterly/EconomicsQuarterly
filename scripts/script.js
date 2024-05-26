4d36c1ad2846e6f30abb3c2d3de96114548dd6d0
ChJus <92497757+ChJus@users.noreply.github.com>
2024-05-26 10:11:34 +0800

    DOM modifications
    Signed-off-by: ChJus <92497757+ChJus@users.noreply.github.com>

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,43 @@
 for (let i = 0; i < document.querySelectorAll(".link").length; i++) {
   document.querySelectorAll(".link")[i].addEventListener("mouseover", function () {
-    colorIssue(`--${i + 1}-primary`, `--${i + 1}-secondary`)
+    colorIssue(i + 1);
   })
 }
 
 for (let i = 0; i < document.querySelectorAll("#colorTiles div").length; i++) {
   document.querySelectorAll("#colorTiles div")[i].addEventListener("mouseover", function () {
-    colorIssue(`--${i + 1}-primary`, `--${i + 1}-secondary`)
+    colorIssue(i + 1);
   })
 }
 
-function colorIssue(primary, secondary) {
-  document.body.style["background-color"] = `var(${secondary})`;
-  document.body.style["color"] = `var(${primary})`;
-  document.querySelector(".subscribe button").style["background-color"] = `var(${primary})`;
-  document.querySelector(".subscribe button").style["color"] = `var(${secondary})`;
+function colorIssue(t) {
+  document.body.style["background-color"] = `var(--${t}-secondary)`;
+  document.body.style["color"] = `var(--${t}-primary)`;
+
   for (let i = 0; i < document.querySelectorAll(".replaceLogo").length; i++) {
-    document.querySelectorAll(".replaceLogo")[i].contentDocument.getElementById("logo").style.color = getComputedStyle(document.body).getPropertyValue(primary);
+    document.querySelectorAll(".replaceLogo")[i].contentDocument.getElementById("logo").style.color = getComputedStyle(document.body).getPropertyValue(`--${t}-primary`);
   }
-  document.querySelector("#footer").style["color"] = `var(${primary})`;
+
+  document.querySelector(".banner").style["background-color"] = `var(--${t}-complement)`;
+  document.querySelector(".banner").style["color"] = `var(--${t}-primary)`;
+
+  document.querySelector("#nav").style["background-color"] = `var(--${t}-primary)`;
+  document.querySelector("#nav").style["color"] = `var(--${t}-secondary)`;
+  document.querySelector("#nav .replaceLogo").contentDocument.getElementById("logo").style.color = getComputedStyle(document.body).getPropertyValue(`--${t}-secondary`);
+
+  document.querySelector(".subscribe button").style["background-color"] = `var(--${t}-primary)`;
+  document.querySelector(".subscribe button").style["color"] = `var(--${t}-secondary)`;
+
+  document.querySelector("#footer").style["color"] = `var(--${t}-primary)`;
+}
+
+var prevScrollpos = window.scrollY;
+window.onscroll = function () {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = `-${document.getElementById("nav").clientHeight}px`;
+  }
+  prevScrollpos = currentScrollPos;
 }
