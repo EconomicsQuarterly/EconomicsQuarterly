f8c5ca8c30fb0d58e858144b5371e740ac354ab1
ChJus <92497757+ChJus@users.noreply.github.com>
2024-07-09 18:11:26 +0800

    changes
    Signed-off-by: ChJus <92497757+ChJus@users.noreply.github.com>

--- 2023-2024/scripts/2023-24highlightsScript.js
+++ 2023-2024/scripts/2023-24highlightsScript.js
@@ -15,7 +15,7 @@ function setup() {
 
   let result = [];
   let k = width / 200;
-  for (let i = 0; i < 207; i++) {
+  for (let i = 0; i < 270; i++) {
     result.push({r: k, group: Math.floor(Math.random() * 4)});
   }
   return result;
